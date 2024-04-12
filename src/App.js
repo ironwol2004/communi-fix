1bf256283b50cb8977cc0bf09724d95257c96ece
IronWol <shrivastavashish2005@gmail.com>
2024-04-12 15:28:08 +0530

    done some more changes

--- src/App.js
+++ src/App.js
@@ -4,20 +4,27 @@ import Home from "./pages/home";
 import Ideas from "./pages/ideas";
 import Form from "./pages/form";
 import About from "./pages/about";
+import axios from "axios";
 
 
 function App() {
-  const [posts,setPosts]=useEffect([]);
+  const [posts,setPosts]=useState([]);
   async function getposts(){
-      
+      const r=await axios.get("https://localhost:5000/getposts");
+      setPosts(r.data);
   }
-  useEffect(()=>{},[]);
+  useEffect(()=>{getposts()},[]);
+  async function addnewpost(newpost){
+    const r = await axios.post("https://localhost:5000/getposts",{post:newpost});
+    getposts();
+    alert(r.data);
+  }
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home></Home>}></Route>
-      <Route path="/ideas" element={<Ideas></Ideas>}></Route>
-      <Route path="/form" element={<Form></Form>}></Route>
+      <Route path="/ideas" element={<Ideas posts={posts}></Ideas>}></Route>
+      <Route path="/form" element={<Form addnewpost={addnewpost}></Form>}></Route>
       <Route path="/about" element={<About></About>}></Route>
     </Routes>
     </BrowserRouter>
--- src/pages/ideas.js
+++ src/pages/ideas.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../common/header";
 import Footer from "../common/footer";
 function Ideas(props){
+    const [cityfilter,setcityfilter]=useState("any");
+    const [categoryfilter,setcategoryfilter]=useState("any");
+    var categories=["any"];
+    var cities=["any"];
+    const p=props.posts;
+    for(var i=0;i<p.length;i++){
+        var a=true;
+        var b=true;
+        for(var j=i+1;j<p.length;j++){
+            if(p[i].city===p[j].city){a=false;}
+            if(p[i].category===p[j].category){
+                b=false;
+            }
+        }
+        if(a===true){cities.concat(p[i].city)}
+        if(b===true){categories.concat(p[i].category)}
+    }
+    function filtering(){
+        var v=props.posts.map((p)=>{if(cityfilter==="any" && categoryfilter==="any"){return p}
+    else if(cityfilter==="any"){if(p.category===categoryfilter){return p}}
+else if(categoryfilter==="any"){if(p.city===cityfilter){return p}}
+else{if(p.category===categoryfilter && p.city===cityfilter){return p}}});
+    }
     return <div>
         <Header></Header>
         This is Ideas page.
